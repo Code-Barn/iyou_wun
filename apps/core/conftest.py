af8875c3d2e2f8b1fe63784aad08b7cd75402cd7
dcbyers13 <dcbyers13@iyou.me>
2026-05-20 16:01:33 -0500

    dba

--- apps/core/conftest.py
+++ apps/core/conftest.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2026 Byers Brands, LLC
+# Copyright (C) 2026 David Byers dba Byers Brands
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
--- manage.py
+++ manage.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-# Copyright (C) 2026 Byers Brands, LLC
+# Copyright (C) 2026 David Byers dba Byers Brands
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
